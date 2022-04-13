fefd089e3244465fe4d999922acb2342786109db
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2022-04-13 17:38:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,12 +25,38 @@ $("button").click(function () {
 function randomSong() {
   var $target = $("#target");
   var item = items[Math.floor(Math.random() * items.length)];
+  var word = words[Math.floor(Math.random() * items.length)];
   $target.html(
-    "<h4 id='my' class='text-center'>" + item + "</h4><p id='para'>"
+    "<h4 id='my' class='text-center'>" +
+      <a href={item}></a> +
+      "</h4><p id='para'>"
   );
 }
 
 var items = Array(
   "https://www.youtube.com/watch?v=l4UkYBr1NnA&ab_channel=Fredagain..",
-  "https://youtu.be/G04fG-4JcrI"
+  "https://youtu.be/G04fG-4JcrI",
+  "https://youtu.be/oZ4Wu-1jBsA",
+  "https://www.youtube.com/watch?v=zPrUtko9jMs",
+  "https://youtu.be/sZd-t5-I5uA",
+  "https://youtu.be/6VFoh5AbpBM",
+  "https://www.youtube.com/watch?v=EtMb1MYjdDQ",
+  "https://www.youtube.com/watch?v=Sp6idnemeE0",
+  "https://www.youtube.com/watch?v=WGbLX4u6kyk",
+  "https://youtu.be/prR9YVj2Ip8",
+  "https://youtu.be/wMRFp9zBBwY",
+  "https://www.youtube.com/watch?v=sOS9aOIXPEk",
+  "https://www.youtube.com/watch?v=q3drqHDn1bs",
+  "https://youtu.be/usuHRa9MNIc",
+  "https://www.youtube.com/watch?v=tidhZ1YBR-A&ab_channel=Elderbrook-Topic",
+  "https://www.youtube.com/watch?v=U2JMCxOmjRk",
+  "https://www.youtube.com/watch?v=cTMucy4PR2k",
 );
+var words = [
+  "hewe you go",
+  "moi",
+  "ilysm ymbf",
+  "broccoli",
+  "sotd",
+  "akulchev",
+];
